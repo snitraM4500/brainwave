88da8013878e5f1a3a20a2bfa2c58b48b6c476cc
Martins_DUamba <martinsduamba@gmail.com>
2024-03-08 16:39:09 +0200

    Header for desktyops ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
-function App() {
-  const [count, setCount] = useState(0);
+import Header from "./components/Header";
 
+function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
 
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          Something
-        </Button>
+        <Header/>
       </div>
       <ButtonGradient />
     </>
