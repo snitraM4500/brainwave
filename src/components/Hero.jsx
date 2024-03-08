be4a139d630965203d1daeba63b9a9a14e9972b0
Martins_DUamba <martinsduamba@gmail.com>
2024-03-08 17:25:16 +0200

    Hero created

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Section from "./Section";
+
+const Hero = () => {
+  return (
+    <Section
+      className={`pt-12 -mt-[5.25]`}
+      crosses
+      crossesOffset={"lg:translate-y-[5.25rem"}
+      customPaddings
+      id={"hero"}>
+      
+    </Section>
+  );
+};
+
+export default Hero;
